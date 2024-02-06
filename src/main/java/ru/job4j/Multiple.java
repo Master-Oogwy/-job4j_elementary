1cd3285e5c623d79735ed995e372614332bd379e
Master-Oogway <nikolay.gorbunkov@yandex.ru>
2024-02-06 11:38:33 +0300

    Разбор первой программы. [#159033 #121751]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.println(i+j);
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikolay Gorbunkov");
+        System.out.println("06.01.1995");
+    }
+}
