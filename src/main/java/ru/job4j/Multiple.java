a8ddf1ba20278ffd7c2555bbaf3333eacdc62b5a
Master-Oogway <nikolay.gorbunkov@yandex.ru>
2024-02-06 11:51:56 +0300

    6. Checkstyle [#4123 #121725]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -4,7 +4,7 @@ public class Multiple {
     public static void main(String[] args) {
         for (int i = 1; i <= 1; i++) {
             for (int j = 0; j < 9; j++) {
-                System.out.println(i+j);
+                System.out.println(i + j);
             }
         }
     }
