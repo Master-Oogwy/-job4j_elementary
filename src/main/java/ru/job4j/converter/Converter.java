4039aaf42cefbaad1e05d8932d44bea59546ed4d
Master-Oogway <nikolay.gorbunkov@yandex.ru>
2024-02-07 14:53:24 +0300

    0. Что такое тестирование [#159431 #121778]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,11 +7,16 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        return value / 63;
+        return value / 60;
     }
 
     public static void main(String[] args) {
         System.out.println(rubleToEuro(1350));
         System.out.println(rubleToDollar(270000));
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToDollar(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 }
